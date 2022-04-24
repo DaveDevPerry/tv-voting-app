3dbb2f0ff7a06c3662bb9268898a0ff820ccd9f9
DaveDevPerry <daveperry.log@gmail.com>
2022-04-24 21:47:24 +0100

    added devTools in index store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -14,12 +14,23 @@ const DEFAULT_STATE = {
   },
 };
 
+const devTools =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
+
 export const store = createStore(
   rootReducer,
   DEFAULT_STATE,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  compose(applyMiddleware(thunk), devTools),
 );
+// export const store = createStore(
+//   rootReducer,
+//   DEFAULT_STATE,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   ),
+// );
