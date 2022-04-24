212d9df92191c534dcaf25ce552b11ebc28718e0
DaveDevPerry <daveperry.log@gmail.com>
2022-04-24 21:52:41 +0100

    added composeWithDevTools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
@@ -14,17 +15,23 @@ const DEFAULT_STATE = {
   },
 };
 
-const devTools =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
+// const devTools =
+//   process.env.NODE_ENV === 'development'
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+//     : null;
 
 export const store = createStore(
   rootReducer,
   DEFAULT_STATE,
-  compose(applyMiddleware(thunk), devTools),
+  composeWithDevTools(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
 );
+
+// WORKING ORIGINAL
 // export const store = createStore(
 //   rootReducer,
 //   DEFAULT_STATE,
