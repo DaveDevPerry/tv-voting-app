d9c076c833ea3e9c2ccb3cc9e172887a57b14f33
DaveDevPerry <daveperry.log@gmail.com>
2022-11-09 20:05:08 +0000

    pre removal of heroku

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-// import { Redirect } from 'react-router-dom';
 
-// import ErrorMessage from '../components/ErrorMessage';
 import Polls from '../components/Polls';
 // import Users from '../components/Users';
 
 const HomePage = (props, isAuthenticated) => {
   // if (!isAuthenticated) return <Redirect to="/login" />;
-  // console.log(props, 'props');
-  // console.log(isAuthenticated, 'auth');
   console.log(props, 'props');
   return (
     <div className="polls-view-container">
       {/* <div className="error-wrapper">
       <ErrorMessage />
     </div> */}
-      {/* <ErrorMessage /> */}
       <Polls {...props} />
       {/* <Users {...props} /> */}
     </div>
